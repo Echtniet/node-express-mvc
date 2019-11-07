1a53fd05a7431b58f2c1616ed5a85bc1d048fd0f
Michael Burens <me@mtburnes.dev>
2019-11-07 00:47:53 -0600

    Updated seeder to include pets.

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,6 +1,6 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
 const developerData = require('../data/developers.json') // read in data file
-
+const petData = require('../data/pets.json')
 // inject Express app to configure it - EVERYTHING in through argument list
 
 module.exports = (app) => {
@@ -8,14 +8,18 @@ module.exports = (app) => {
   const db = {} // empty object to hold all collections
 
   db.developers = new Datastore() // new object property
+  db.pets = new Datastore()
   db.developers.loadDatabase() // call the loadDatabase method
-
+  
   // insert the sample data into our datastore
   db.developers.insert(developerData)
+  db.pets.insert(petData)
 
   // initialize app.locals (these objects are available to the controllers)
   app.locals.developers = db.developers.find(developerData)
+  app.locals.pets = db.pets.find(petData)
   console.log(`${app.locals.developers.query.length} developers seeded`)
+  console.log(`${app.locals.pets.query.length} pets seeded`)
 
   console.log('END Data Seeder. Sample data read and verified.')
 }
