d4ba6d4078cf09ecf485fb84005bba0122d8bb76
Michael Burens <me@mtburnes.dev>
2019-11-06 23:56:37 -0600

    Finished pets.js
    I considered adding 'isGoodBoy'. This is redundant data, as all pets are good boys regardless of sex.

--- models/pets.js
+++ models/pets.js
@@ -10,6 +10,33 @@ const mongoose = require('mongoose')
 
 const PetsSchema = new mongoose.Schema({
 
+    _id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        minlength: 3,
+        maxlength: 30, //any names longer than this causes a confused animal. The developers will also be very confused.
+        required: true,
+        default: 'Name Error'
+    },
+    owner: {
+        type: Student,
+        required: true,
+    },
+    birthDate: {
+        type: Date,
+        required: true, //pet birthdays are essential. No exceptions.
+    },
+    breed: {
+        type: String,
+        required: false,
+    },
+    friendly: {
+        type: Boolean,
+        required: true,
+    }
 })
 
 module.exports = mongoose.model('Pets', PetsSchema)
