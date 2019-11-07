2c6b3a6548b0c4cdf9e409f044f5473eac193199
Michael Burnes <43098443+mtburnes@users.noreply.github.com>
2019-11-06 23:37:09 -0600

    Skeleton Pets Model

--- models/pets.js
+++ models/pets.js
@@ -1,1 +1,15 @@
+/**
+*  Pets model
+*  Describes the characteristics of each attribute in a pets resource.
+*
+* @author Michael Burnes <me@mtburnes.dev>
+* @requires mongoose
+*
+*/
+const mongoose = require('mongoose')
 
+const PetsSchema = new mongoose.Schema({
+
+})
+
+module.exports = mongoose.model('Pets', PetsSchema)
