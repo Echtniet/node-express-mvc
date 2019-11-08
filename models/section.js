7f4fac8274332dc45bfe106408d54b07cce5aed0
ackin <colehouston44@gmail.com>
2019-11-08 11:48:44 -0600

    created section.js

---
+++ models/section.js
@@ -0,0 +1,32 @@
+/**
+*  Section model
+*  Describes the characteristics of each attribute in the Section resource.
+*
+* @author Cole Houston <s522101@nwmissouri.edu>
+* @requires mongoose
+*
+*/
+//reference teacher id, course id, student id
+
+const mongoose = require('mongoose')
+const SectionSchema = new mongoose.Schema({
+
+    _id: {
+	type: Number,
+	required: true
+    },
+    courseId: {
+	type: Number
+	required: true
+    },
+    professorId{
+	type: Number,
+	required: true
+    },
+    studentId{
+	type: Number,
+	required: true
+    }
+})
+
+module.exports = mongoose.model('Section', SectionSchema)
