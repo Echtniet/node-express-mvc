a5adb4b0a6738dea8affde4e45991d50e56c2bc7
ackin <colehouston44@gmail.com>
2019-11-08 20:51:37 -0600

    added section.js sections

--- utils/seeder.js
+++ utils/seeder.js
@@ -3,6 +3,7 @@ const developerData = require('../data/developers.json') // read in data file
 const petData = require('../data/pets.json')
 const studentData = require('../data/students.json')
 const courseData = require('../data/course.json')
+const sectionData = require('../data/section.json')
 const teacherData = require('../data/teachers.json')
 // inject Express app to configure it - EVERYTHING in through argument list
 
@@ -14,11 +15,13 @@ module.exports = (app) => {
   db.teachers = new Datastore()
   db.students = new Datastore()
   db.course = new Datastore()
+  db.section = new Datastore()
   db.pets = new Datastore()
   //db.developers.loadDatabase()
   db.teachers.loadDatabase() // call the loadDatabase method
   db.students.loadDatabase()
   db.course.loadDatabase()
+  db.section.loadDatabase()
   db.pets.loadDatabase()
   
   // insert the sample data into our datastore
@@ -26,6 +29,7 @@ module.exports = (app) => {
   db.teachers.insert(teacherData)
   db.students.insert(studentData)
   db.course.insert(courseData)
+  db.section.insert(sectionData)
   db.pets.insert(petData)
 
   // initialize app.locals (these objects are available to the controllers)
@@ -33,12 +37,14 @@ module.exports = (app) => {
   app.locals.teachers = db.course.find(teacherData)
   app.locals.students = db.students.find(studentData)
   app.locals.course = db.course.find(courseData)
+  app.locals.section = db.section.find(sectionData)
   app.locals.pets = db.pets.find(petData)
   
   //console.log(`${app.locals.developers.query.length} developers seeded`)
   console.log(`${app.locals.teachers.query.length} teachers seeded`)
   console.log(`${app.locals.students.query.length} students seeded`)
   console.log(`${app.locals.course.query.length} course seeded`)
+  console.log(`${app.locals.section.query.length} section seeded`)
   console.log(`${app.locals.pets.query.length} pets seeded`)
 
   console.log('END Data Seeder. Sample data read and verified.')
