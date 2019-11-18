8f703dedaf80e3be7b846dad2759b106674846b5
Clinton Davelaar <DavelaarProgramming@gmail.com>
2019-11-18 13:01:01 -0600

    everything is working

--- controllers/teachers.js
+++ controllers/teachers.js
@@ -70,10 +70,15 @@ api.get('/delete/:id', (req, res) => {
 })
 
 // GET edit
-  api.get('/edit', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  return res.end("Edit request")
-  res.send(JSON.stringify(item))
+api.get('/edit/:id', (req, res) => {
+  LOG.info(`Handling GET /edit/:id ${req}`)
+  const id = parseInt(req.params.id)
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR${JSON.stringify(results)}`)
+    res.locals.teacher = results[0]
+    return res.render('teachers/edit.ejs')
+  })
 })
 
 // GET create
@@ -93,10 +98,62 @@ api.post('/postcreate', (req, res) =>{
   res.send(JSON.stringify(item))
 })
 
+
+
 // CRUD METHODS
-function handleCreate(){}
-function handleUpdate(){}
-function handleDelete(){}
+api.post('/save', (req, res) => {
+  LOG.info(`Handling POST ${req}`)
+  LOG.debug(JSON.stringify(req.body))
+  const item = new Model()
+  LOG.info(`NEW ID ${req.body._id}`)
+  item._id = parseInt(req.body._id)
+  item.email = req.body.email
+  item.given = req.body.given
+  item.family = req.body.family
+  item.salary = req.body.salary
+  item.github = req.body.github
+  item.save((err) => {
+    if (err) { return res.end('ERROR: item could not be saved') }
+    LOG.info(`SAVING NEW item ${JSON.stringify(item)}`)
+    return res.redirect('/teachers/index')
+  })
+})
+
+// POST save with id
+api.post('/save/:id', (req, res) => {
+  LOG.info(`Handling SAVE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling SAVING ID=${id}`)
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        email: req.body.email,
+        given: req.body.given,
+        family: req.body.family,
+        salary: req.body.salary,
+        githb: req.body.github
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED item ${JSON.stringify(item)}`)
+      return res.redirect('/teachers/index')
+    })
+})
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', (req, res) => {
+  LOG.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling REMOVING ID=${id}`)
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/teachers/index')
+  })
+})
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
