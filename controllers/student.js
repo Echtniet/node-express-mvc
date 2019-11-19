2f1bd3c4633d1eb6bd12579c19c34f15f0529c41
Ben Brown <46798091+BenB049@users.noreply.github.com>
2019-11-18 20:00:43 -0600

    updated student controller

--- controllers/student.js
+++ controllers/student.js
@@ -66,9 +66,63 @@ api.get('/create', (req, res) => {
 })
 
 // CRUD METHODS
-function handleCreate(){}
-function handleUpdate(){}
-function handleDelete(){}
+api.post('/save', (req, res) => {
+  LOG.info(`Handling POST ${req}`)
+  LOG.debug(JSON.stringify(req.body))
+  const item = new Model()
+  LOG.info(`NEW ID ${req.body._id}`)
+  item._id = parseInt(req.body._id)
+  item.given = req.body.given
+  item.family = req.body.family
+  item.email = req.body.email
+  item.gpa = req.body.gpa
+  item.github = req.body.github
+  item.website = req.body.website
+  item.sectionID = req.body.sectionID
+  item.save((err) => {
+    if (err) { return res.end('ERROR: item could not be saved') }
+    LOG.info(`SAVING NEW item ${JSON.stringify(item)}`)
+    return res.redirect('/students/index')
+  })
+})
+
+// POST save with id
+api.post('/save/:id', (req, res) => {
+  LOG.info(`Handling SAVE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling SAVING ID=${id}`)
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        given: req.body.given,
+        family: req.body.family,
+        email: req.body.email,
+        gpa: req.body.gpa,
+        githb: req.body.github,
+        website: req.body.website,
+        sectionID: req.body.sectionID
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED item ${JSON.stringify(item)}`)
+      return res.redirect('/students/index')
+    })
+})
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', (req, res) => {
+  LOG.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling REMOVING ID=${id}`)
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/students/index')
+  })
+})
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
