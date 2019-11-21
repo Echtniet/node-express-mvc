db6241aa6297695c635bdce0558edb5c61a25ad3
Clinton Davelaar <DavelaarProgramming@gmail.com>
2019-11-21 09:19:26 -0600

    almost done

--- controllers/section.js
+++ controllers/section.js
@@ -7,7 +7,8 @@
 */
 const express = require('express')
 const api = express.Router()
-// const Model = require('../models/section.js')
+const Model = require('../models/section.js')
+const LOG = require('../utils/logger.js')
 const find = require('lodash.find')
 const notfoundstring = 'Could not find section with id='
 
@@ -15,61 +16,143 @@ const notfoundstring = 'Could not find section with id='
 
 // GET all JSON
 api.get('/findall', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  const data = req.app.locals.section.query
-  res.send(JSON.stringify(data))
+  LOG.info(`Handling /findall ${req}`)
+  Model.find({}, (err, data) => {
+    if (err) { return res.end('Error finding all') }
+    res.json(data)
+  })
 })
 
 // GET one JSON by ID
 api.get('/findone/:id', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
+  LOG.info(`Handling /findone ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.section.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.send(JSON.stringify(item))
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(`notfoundstring ${id}`) }
+    res.json(results[0])
+  })
 })
 
 // GET index
 api.get('/index', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  return res.end("Index request")
-  res.send(JSON.stringify(item))
+  LOG.info(`Handling GET / ${req}`)
+  Model.find({}, (err, data) => {
+    if (err) { return res.end('Error') }
+    res.locals.sections = data
+    res.render('section/index.ejs')
+  })
 })
 
 // GET details
-api.get('/details', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  return res.end("Detail request")
-  res.send(JSON.stringify(item))
+api.get('/details/:id', (req, res) => {
+  LOG.info(`Handling GET /details/:id ${req}`)
+  const id = parseInt(req.params.id)
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.section = results[0]
+    return res.render('section/details.ejs')
+  })
 })
 
 // GET delete
-api.get('/delete', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  return res.end("Delete request")
-  res.send(JSON.stringify(item))
+api.get('/delete/:id', (req, res) => {
+  LOG.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling REMOVING ID=${id}`)
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/section/index')
+  })
 })
 
 // GET edit
-  api.get('/edit', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  return res.end("Edit request")
-  res.send(JSON.stringify(item))
+api.get('/edit/:id', (req, res) => {
+  LOG.info(`Handling GET /edit/:id ${req}`)
+  const id = parseInt(req.params.id)
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR${JSON.stringify(results)}`)
+    res.locals.section = results[0]
+    return res.render('section/edit.ejs')
+  })
 })
 
 // GET create
 api.get('/create', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  return res.end("Create request")
-  res.send(JSON.stringify(item))
+  LOG.info(`Handling GET /create ${req}`)
+  Model.find({}, (err, data) => {
+    if (err) { return res.end('error on create') }
+    res.locals.sections = data
+    res.locals.section = new Model()
+    res.render('section/create')
+  })
 })
 
 // CRUD METHODS
-function handleCreate(){}
-function handleUpdate(){}
-function handleDelete(){}
+api.post('/postcreate', (req, res) =>{
+  res.setHeader('Content-Type', 'application/json')
+  return res.end("Post create request")
+  res.send(JSON.stringify(item))
+})
 
+
+
+// CRUD METHODS
+api.post('/save', (req, res) => {
+  LOG.info(`Handling POST ${req}`)
+  LOG.debug(JSON.stringify(req.body))
+  const item = new Model()
+  LOG.info(`NEW ID ${req.body._id}`)
+  item._id = parseInt(req.body._id)
+  item.email = req.body.email
+  item.given = req.body.given
+  item.family = req.body.family
+  item.salary = req.body.salary
+  item.github = req.body.github
+  item.save((err) => {
+    if (err) { return res.end('ERROR: item could not be saved') }
+    LOG.info(`SAVING NEW item ${JSON.stringify(item)}`)
+    return res.redirect('/teachers/index')
+  })
+})
+
+// POST save with id
+api.post('/save/:id', (req, res) => {
+  LOG.info(`Handling SAVE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling SAVING ID=${id}`)
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        email: req.body.email,
+        given: req.body.given,
+        family: req.body.family,
+        salary: req.body.salary,
+        githb: req.body.github
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED item ${JSON.stringify(item)}`)
+      return res.redirect('/teachers/index')
+    })
+})
+
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', (req, res) => {
+  LOG.info(`Handling DELETE request ${req}`)
+  const id = parseInt(req.params.id)
+  LOG.info(`Handling REMOVING ID=${id}`)
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/teachers/index')
+  })
+})
 // RESPOND WITH VIEWS  --------------------------------------------
 
 // later
